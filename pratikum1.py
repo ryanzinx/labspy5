81edd7efea947fc25e7fc08ec8aa85a77ae17b4d
ryanzinx <92358422+ryanzinx@users.noreply.github.com>
2021-12-06 01:11:55 +0700

    Update pratikum1.py

--- pratikum1.py
+++ pratikum1.py
@@ -1,26 +1,78 @@
-listNama=[]
-listNim=[]
-listTugas=[]
-listUts=[]
-listUas=[]
-listAkhir=[]
+a = {}
 
-a=0
-while a==0:
-    listNama.append(input("Nama: "))
-    listNim.append(input("Nim: "))
-    listTugas.append(input("Nilai Tugas: "))
-    listUts.append(input("Nilai UTS: "))
-    listUas.append(input("Nilai UAS: "))
-    a=(input("Tambah Data (y/t)? "))
-    if a=="y":
-        a=0
-    elif a=="t":
-        listAkhir.append((int(tugas) * .30) + (int(uts) * .35) + (int(uas) * .35))
+while True:
+    x = input("[(T)ambah, (U)bah, (H)apus, (C)ari, (L)ihat, (K)eluar]: ")
 
-print("\nNama:",nama)
-print("\nNIM:",nim)
-print("Nilai UTS:",uts)
-print("Nilai UAS:",uas)
-print("Nilai Tugas:",tugas)
-print("Nilai Akhir:",akhir)
+    if x.lower() == 't':
+        print("Tambah Data")
+        nama = input("Nama           : ")
+        nim = int(input("NIM            : "))
+        uts = int(input("Nilai UTS      : "))
+        uas = int(input("Nilai UAS      : "))
+        tugas = int(input("Nilai Tugas    : "))
+        n_akhir = tugas * 0.30 + uts * 0.35 + uas * 0.35
+        a[nama] = nim, uts, uas, tugas, n_akhir
+
+    elif x.lower() == 'u':
+        print("Ubah Data")
+        nama = input("Masukkan Nama   : ")
+        if nama in a.keys():
+            nim = int(input("NIM            : "))
+            uts = int(input("Nilai UTS      : "))
+            uas = int(input("Nilai UAS      : "))
+            tugas = int(input("Nilai Tugas    : "))
+            n_akhir = tugas*0.30 + uts*0.35 + uas*0.35
+            a[nama] = nim, uts, uas, tugas, n_akhir
+        else:
+            print("Nama{0} Tidak Ditemukan".format(nama))
+
+    elif x.lower() == 'h':
+        print("Hapus Data")
+        nama = input("Masukkan Nama  : ")
+        if nama in a.keys():
+            del a[nama]
+        else:
+            print("Nama {0} Tidak Ditemukan".format(nama))
+
+    elif x.lower() == 'c':
+        print("Cari Data")
+        nama = input("Masukkan Nama : ")
+        if nama in a.keys():
+            print("=" * 73)
+            print("|                             Daftar Mahasiswa                          |")
+            print("=" * 73)
+            print("| Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("=" * 73)
+            print("| {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                  .format(nama, nim, uts, uas, tugas, n_akhir))
+            print("=" * 73)
+        else:
+            print("Nama {0} Tidak Ditemukan".format(nama))
+
+    elif x.lower() == 'l':
+        if a.items():
+            print("=" * 78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("=" * 78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("=" * 78)
+            i = 0
+            for y in a.items():
+                i += 1
+                print("| {no:2d} | {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                      .format(y[0][:13], y[1][0], y[1][1], y[1][2], y[1][3], y[1][4], no=i))
+                print("=" * 78)
+        else:
+            print("=" * 78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("=" * 78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("=" * 78)
+            print("|                                TIDAK ADA DATA                              |")
+            print("=" * 78)
+
+    elif x.lower() == 'k':
+        break
+
+    else:
+        print("Pilih Menu Yang Tersedia")
